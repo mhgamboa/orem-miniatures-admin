17c8f294d6a6b45471cd589b7ac213c42b1564ed
Marcus Gamboa <marcus.gamboa@gmail.com>
2024-08-16 12:19:41 -0600

    Add shadcn Form to store-modal

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,14 +1,58 @@
 "use client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Modal } from "@/components/ui/modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+});
 
 export default function StoreModal() {
   const isOpen = useStoreModal(s => s.isOpen);
   const onClose = useStoreModal(s => s.onClose);
+
+  const form = useForm<z.infer<typeof formSchema>>({ resolver: zodResolver(formSchema), defaultValues: { name: "" } });
+
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    // TODO: Create store
+    console.log(value);
+  };
+
   return (
     <Modal title="Create store" description="Description" isOpen={isOpen} onClose={onClose}>
-      Future Create Store Form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                name="name"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="E-Commerce" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant="outline" onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 }
