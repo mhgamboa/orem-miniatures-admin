285c5c549e7f342597b3738874332cfaf83e7445
Marcus Gamboa <marcus.gamboa@gmail.com>
2024-08-23 14:15:43 -0600

    Add functionality to add Stores from Nav
    Use Shadcn combobox

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,7 +1,31 @@
 "use client";
 
 import React from "react";
+import { useParams, usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElement>) {
-  return <p>Main navigation</p>;
+  const pathName = usePathname();
+  const params = useParams();
+
+  const routes = [{ href: `/${params.storeId}/settings`, label: "Settings", active: pathName === `/${params.storeId}/settings` }];
+
+  return (
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+      {routes.map(route => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active ? "text-black dark:text-white" : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,26 @@
-import { UserButton } from "@clerk/nextjs";
 import React from "react";
-import MainNav from "@/components/MainNav";
 
-export default function Navbar() {
+import MainNav from "@/components/MainNav";
+import StoreSwitcher from "@/components/StoreSwitcher";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import prismaDb from "@/lib/prismadb";
+
+export default async function Navbar() {
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+
+  const stores = await prismaDb.store.findMany({
+    where: {
+      userId: userId,
+    },
+  });
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <div>This will be a store switcher</div>
-        <MainNav />
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton />
         </div>
