0d2cc14226269d2a78dd243cde1f9c401d9011c2
Marcus Gamboa <marcus.gamboa@gmail.com>
2024-08-29 14:54:17 -0600

    Add Categories to MainNav

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -13,6 +13,7 @@ export default function MainNav({ className, ...props }: React.HTMLAttributes<HT
   const routes = [
     { href: `/${params.storeId}`, label: "Overview", active: pathName === `/${params.storeId}` },
     { href: `/${params.storeId}/billboards`, label: "Billboards", active: pathName === `/${params.storeId}/billboards` },
+    { href: `/${params.storeId}/categories`, label: "Categories", active: pathName === `/${params.storeId}/categories` },
     { href: `/${params.storeId}/settings`, label: "Settings", active: pathName === `/${params.storeId}/settings` },
   ];
 
